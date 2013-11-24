9833a1812c9b8b6889c657661ee2f0ae50317348
Hans Meyer <git@hansmeyer.com>
2013-11-24 19:52:03 +0000

    initial

---
+++ js/originEvents.js
@@ -0,0 +1,47 @@
+(function(undefined) {
+	var count = 0;
+	var baseKeyName = 'originEvents.nooxis.com.';
+	var windowKeyName = baseKeyName + uuid.v4() + '.';
+	var re = new RegExp('^' + escapeRegExp(baseKeyName) + '[0-9a-f]{32}\\.\\d+');
+	var handler;
+
+	window.addEventListener('storage', function(e) {
+		if (handler && e.newValue !== null && re.test(e.key)) {
+			// console.log('originEvents (different window):', e.key, snorkel.decodeValue(e.newValue));
+			handler.call(snorkel.decodeValue(e.newValue), false);
+		}
+	}, false);
+
+	snorkel.on(function(e, k, v) {
+		if (handler && (e === 'updated' || e === 'added')) {
+			// console.log('originEvents (same window):', k, v);
+			handler.call(v, true);
+			snorkel.remove(k);
+		}
+	}, re);
+
+	var originEvents = {
+		trigger: function(iType, iMessage) {
+			snorkel.set(windowKeyName + count, {
+				//count: ++count,
+				datetime: new Date(),
+				type: iType,
+				message: iMessage
+			});
+		},
+		on: function(iHandler) {
+			if (_.isFunction(iHandler)) {
+				handler = iHandler;
+			}
+		},
+		off: function() {
+			handler = null;
+		}
+	};
+
+	function escapeRegExp(iStr) {
+		return iStr.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+	}
+
+	window.originEvents = originEvents;
+}).call(this);
